8785467da657cd4995b1b857b00919c1a574b7bd
MarjuDox <nguyenson09082017@gmail.com>
2022-12-14 17:02:35 +0700

    .

---
+++ Final_Project/src/main/java/com/example/demo/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import com.example.demo.model.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	@Query("SELECT u FROM Product u WHERE u.category_name = ?1")
+	Product findbyCategory_name(String category_name);
+}
