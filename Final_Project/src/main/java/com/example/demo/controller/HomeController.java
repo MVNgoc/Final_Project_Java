fe55516ce5ae6607ce91830ce342b03b6027d7e7
HungNghia2010 <93834571+HungNghia2010@users.noreply.github.com>
2022-12-15 19:23:26 +0700

    update

--- Final_Project/src/main/java/com/example/demo/controller/HomeController.java
+++ Final_Project/src/main/java/com/example/demo/controller/HomeController.java
@@ -227,7 +227,8 @@ public class HomeController {
 	
 	//xử lý thêm món
 	@PostMapping("/menu_add")
-	public String addFood(HttpServletRequest request,@RequestParam("img-food-input") MultipartFile multipartFile) throws IOException {
+	public String addFood(HttpServletRequest request,@RequestParam("img-food-input") MultipartFile multipartFile
+			,Model model) throws IOException {
 		Product product = new Product();
 		String type = request.getParameter("type");
 		String typeV;
@@ -260,10 +261,10 @@ public class HomeController {
 		} catch (IOException e) { 
 			throw new IOException("Could not save uploaded file: " + fileName); 
 		}
-		 
+		model.addAttribute("success", "Thêm món thành công"); 
 		
 
-		return "redirect:/menu";
+		return "menu";
 	}
 	
 	@GetMapping("/logout")
