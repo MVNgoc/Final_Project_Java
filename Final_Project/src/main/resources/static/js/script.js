bf739e231da117ffd7c2aaba397d75e1e703cc3f
MaiNgoc <mvn.bp01@gmail.com>
2022-12-16 11:26:17 +0700

    fix js popup 3 page admin

--- Final_Project/src/main/resources/static/js/script.js
+++ Final_Project/src/main/resources/static/js/script.js
@@ -207,12 +207,15 @@ $(document).ready(function() {
 	
 	var img_food_editinput = document.getElementById("img-food-editinput");
 	var img_food_edit = document.getElementById("img-food-edit");
-	img_food_editinput.onchange = e => {
-		const [file] = img_food_editinput.files;
-		if(file){
-			img_food_edit.src = URL.createObjectURL(file);
-		}
-	};
+	
+	if(img_food_editinput != null) {	
+		img_food_editinput.onchange = e => {
+			const [file] = img_food_editinput.files;
+			if(file){
+				img_food_edit.src = URL.createObjectURL(file);
+			}
+		};
+	}
 	
 
 	/* Preview an image before it is uploaded */
